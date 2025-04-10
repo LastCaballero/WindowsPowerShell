0ecd861f57459592eb07245cbfdeaca82d95a565
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2025-04-10 20:14:14 +0200

    Add files via upload

---
+++ matrix.ps1
@@ -0,0 +1,26 @@
+$Colors = "Black", "Cyan", "DarkCyan", "DarkGreen", "DarkRed", "Gray", "Magenta", "White",
+"Blue", "DarkBlue", "DarkGray", "DarkMagenta", "DarkYellow", "Green", "Red", "Yellow"
+
+function Random-Char {
+	"0123456789abcdef".ToCharArray() | Get-Random
+}
+
+
+function Line {
+	while ( $i -le $Width ) {
+		$Width 	=[System.Console]::WindowWidth
+		$Exp 	= " $( Random-Char )$( Random-Char )"
+		$RandomColor = $Colors | Get-Random
+		$Exp.ToCharArray().ForEach{	
+			Write-Host -ForegroundColor $RandomColor -NoNewline $_
+			$i++
+			if ( $i -ge $Width ) {
+				break
+			}	
+		}
+	}
+}
+
+while ( 1 ) {
+	Line
+}
