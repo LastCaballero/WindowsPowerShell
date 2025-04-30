9aab7241f1de2e4170bb9a38c262acd7056bb106
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2025-04-30 14:13:58 +0200

    Create LinuxTools.psm1

---
+++ LinuxTools/LinuxTools.psm1
@@ -0,0 +1,44 @@
+function Pager {
+	begin {
+		$Lines = [System.Collections.ArrayList]::new()
+		$LinesToDisplay = [System.Console]::WindowHeight - 2
+		$LineStart = 0
+	}
+
+	process {
+		$Lines.Add( $_ )
+	}
+
+	end {
+		$i = 0
+		$exit = $false
+		for ( $i -le $Lines.Count ) {
+			Clear-Host
+			"`n"
+			$Lines[ $i .. ( $i + $LinesToDisplay ) ]
+			"space , (b)ack, uparrow, downarrow, (e)xit"
+			$key = [System.Console]::ReadKey()
+			switch ( $key.Key ) {
+				{ $_ -eq "Spacebar" }{ 
+					$i += $LinesToDisplay
+				}
+				{ $_ -eq "b" }{ 
+					$i -= $LinesToDisplay
+				}
+				{ $_ -eq "UpArrow" }{
+					$i -= 1
+				}
+				{$_ -eq "DownArrow"}{
+					$i += 1
+				}
+				{$_ -eq "e"}{
+					$exit = $true ; break ;
+				}
+			}
+			if ( $exit ) { break }
+			
+			
+		}
+		
+	}
+}
